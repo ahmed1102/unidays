388e809f46ddf3bba60c276f81b660f292c762d8
ahmed1102 <Ahmed.tayfour@hotmail.co.uk>
2017-09-17 21:49:29 +0100

    full working web applicaiton

---
+++ UnidaysTestAhmedAhmed/Controllers/UserController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UnidaysTestAhmedAhmed.Models;
+
+namespace UnidaysTestAhmedAhmed.Controllers
+{
+    public class UserController : Controller
+    {
+        //
+        // GET: /User/
+        public ActionResult index()
+        {
+
+            UserInfoEntities db = new UserInfoEntities();
+            //User user = new User();
+
+            //UserViewModel uvm = new UserViewModel();
+
+            return View();
+        }
+        [HttpPost]
+        // SET: /User/
+        public ActionResult index(UserViewModel model)
+        {
+
+            if (ModelState.IsValid)
+            {
+                UserInfoEntities db = new UserInfoEntities();
+
+                User user = new User();
+
+                int userId = user.UserId;
+                user.EmailAddress = model.EmailAddress;
+                user.Password = model.Password;
+
+                db.Users.Add(user);
+
+                db.SaveChanges();
+
+                
+                // Submit values to the database
+                return RedirectToAction("index");
+            }
+
+            return View(model);
+            
+        }
+
+	}
+}
---
+++ UnidaysTestAhmedAhmed/Models/UserViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace UnidaysTestAhmedAhmed.Models
+{
+    public class UserViewModel
+    {
+        public int UserId { get; set; }
+
+        [Display(Name = "EmailAddress")]
+        [Required(ErrorMessage = "Email Address is required")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
+        public string EmailAddress { get; set; }
+
+        [Display(Name = "Password")]
+        [Required(ErrorMessage = "Password is required")]
+        [StringLength(255, ErrorMessage = "Must be between 5 and 255 characters", MinimumLength = 5)]
+        public string Password { get; set; }
+
+      
+
+    }
+}
